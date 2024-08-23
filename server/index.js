b5b101e8b2f023298994a6224f1b3f6220255202
DHARMA TEJA <dteja2468@gmail.com>
2024-08-23 23:42:11 +0530

    main

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const teamRoutes = require('./routes/teamRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5000;
 
 
 // Middleware
@@ -13,7 +13,7 @@ app.use(express.json()); // To handle JSON data
 
 // Routes
 app.use('/api/teams', teamRoutes);
-const paymentRoutes = require('./routes/paymentRoutes');
+const paymentRoutes = require('./routes/paymentRoutes.js');
 app.use('/api/payment', paymentRoutes);
 
 
